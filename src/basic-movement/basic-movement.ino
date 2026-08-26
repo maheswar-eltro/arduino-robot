da3fea80ab8aab64c0b2030d3121a95863b24b44
Maheswar <maheswarsibi@gmail.com>
2026-08-27 01:12:50 +0530

    Add Forward, Reverse, Left, Right and Stop

--- src/basic-movement/basic-movement.ino
+++ src/basic-movement/basic-movement.ino
@@ -8,8 +8,8 @@ const int BIN1 = 9;
 const int BIN2 = 10;
 const int PWMB = 11;
 
-
 void setup() {
+
   pinMode(AIN1, OUTPUT);
   pinMode(AIN2, OUTPUT);
   pinMode(PWMA, OUTPUT);
@@ -20,83 +20,81 @@ void setup() {
   pinMode(BIN1, OUTPUT);
   pinMode(BIN2, OUTPUT);
   pinMode(PWMB, OUTPUT);
-  
-  Serial.begin(9600);
 }
 
 void loop() {
-  if (Serial.available()) {
 
-    char command = Serial.read();
+  move_front();
+  delay(1000);
 
-    if (command == 'w') {
-      move_front();
-    }
-    else if (command == 's') {
-      move_back();
-    }
-    else if (command == 'a') {
-      turn_left();
-    }
-    else if (command == 'd') {
-      turn_right();
-    }
-    else if (command == 'x') {
-      stop();
-    }
-  }
+  stop();
+  delay(500);
+
+  turn_left();
+  delay(1000);
+
+  stop();
+  delay(500);
+
+  turn_right();
+  delay(1000);
+
+  stop();
+  delay(500);
+
+  move_back();
+  delay(1000);
+
+  stop();
+  delay(500);
 }
 
+void move_front() {
 
-void move_front (){
   digitalWrite(AIN1, HIGH);
   digitalWrite(AIN2, LOW);
-  
   analogWrite(PWMA, 130);
 
   digitalWrite(BIN1, HIGH);
   digitalWrite(BIN2, LOW);
-  
   analogWrite(PWMB, 255);
 }
 
-void move_back (){
+void move_back() {
+
   digitalWrite(AIN1, LOW);
   digitalWrite(AIN2, HIGH);
-  
-  analogWrite(PWMA, 150);
+  analogWrite(PWMA, 130);
 
   digitalWrite(BIN1, LOW);
   digitalWrite(BIN2, HIGH);
-  
   analogWrite(PWMB, 255);
 }
 
-void turn_left () {
+void turn_left() {
+
   digitalWrite(AIN1, HIGH);
   digitalWrite(AIN2, LOW);
-  
   analogWrite(PWMA, 130);
 
   digitalWrite(BIN1, LOW);
   digitalWrite(BIN2, LOW);
-  
   analogWrite(PWMB, 0);
 }
 
 void turn_right() {
+
   digitalWrite(AIN1, LOW);
   digitalWrite(AIN2, LOW);
-  
   analogWrite(PWMA, 0);
 
   digitalWrite(BIN1, HIGH);
   digitalWrite(BIN2, LOW);
-  
   analogWrite(PWMB, 255);
 }
 
 void stop() {
+
   analogWrite(PWMA, 0);
   analogWrite(PWMB, 0);
 }
